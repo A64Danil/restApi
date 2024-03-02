63f0d843a596a336cf8eb68c983f8267018f8579
DanilZen <a64danil@mail.ru>
2024-03-02 18:29:27 +0100

    add replaceAll polyfill to UTILS

--- utils.js
+++ utils.js
@@ -31,7 +31,15 @@ function getPostData(req) {
     })
 }
 
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
 module.exports = {
     writeDataToFile,
+    replaceAll,
     getPostData
 }
