e8ece1336143cc428407557517e50d220feb05c5
DanilZen <a64danil@mail.ru>
2024-03-08 17:20:06 +0100

    add space between functions

--- utils.js
+++ utils.js
@@ -34,6 +34,7 @@ function getPostData(req) {
 function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 }
+
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
 }
