c7a9c0113a11af1869177d4049b6385afef0baeb
DanilZen <a64danil@mail.ru>
2024-03-22 18:55:15 +0100

    add main files to config server

---
+++ config/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// TODO: сомнительо что тут это нужно
+const _ = require('lodash');
+const env = process.env.NODE_ENV || 'local';
+const envConfig = require('./' + env);
+
+let defaultConfig = {
+    env: env
+};
+
+module.exports = _.merge(defaultConfig, envConfig);
