935ea19a3445a87b7767e5271b17540e9855af0f
DanilZen <a64danil@mail.ru>
2024-03-31 19:53:01 +0200

    try to add ActiveRecordEntity (just crete files)

---
+++ server/models/ActiveRecordEntity.js
@@ -0,0 +1,25 @@
+let db = require('../services/db')
+
+class ActiveRecordEntity {
+
+    /** @var int */
+    id;
+
+
+    constructor() {
+        this.id = 0;
+    }
+
+
+    /**
+
+     * @return int
+
+     */
+
+    // function getId(){
+    //
+    //     return $this->id;
+    //
+    // }
+}
