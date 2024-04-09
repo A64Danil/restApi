d75cee4d7561cbda6930af882839995e0685bd02
DanilZen <a64danil@mail.ru>
2024-04-09 11:16:20 +0200

    change msql to msql2 connection in DB.js

--- server/services/db.js
+++ server/services/db.js
@@ -1,9 +1,9 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const DBconfig = require('../../dbEnv.json');
+// TODO: old version of connection
+// const connection = mysql.createConnection(DBconfig)
 
-const connection = mysql.createConnection(DBconfig)
+const connection = mysql.createPool(DBconfig).promise()
 
-connection.connect();
-
-module.exports = connection;
+module.exports = { conn: connection };
