2396e896f9884522f3312d250a2a02b7e489b7df
DanilZen <a64danil@mail.ru>
2024-04-09 10:39:26 +0200

    change path to db-config

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const server = require('./server')();
-const config = require('./configs');
-const db = require('./configs/db');
+const config = require('./config');
+// TODO: возможно стоит вынести в какое-то отдельное место
+// const db = require('./config/db');
+const db = require('./dbEnv.json');
 
 server.create(config, db);
 server.start();
