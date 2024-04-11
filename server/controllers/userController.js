4777036072efac0d79f84e81372bc80411a3265a
DanilZen <a64danil@mail.ru>
2024-04-11 10:53:42 +0200

    add first errorHandler to userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,55 @@
 'use strict';
 
 const express = require('express');
+const errorHandler = require('../../utils/errorHandler');
 const userService = require('../services/user/user');
+
+const AppError = require('../../utils/appError');
 
 let router = express.Router();
 
 // TODO: перенести в все обработки res.status из сервисов - сюда
 
-router.get('/', userService.getAllUsers);
+// res.status(200).json({
+//     status: "success",
+//     length: users?.length,
+//     data: users,
+// });
+
+// TODO: попробовать использовать errorHandler
+router.get('/', async (req, res) => {
+    try {
+        const users = await userService.getAllUsers();
+        if (!users || !users.length) {
+            // res.status(404).json({ error: 'Users not found!' });
+            const err = new AppError('Users not found', 404);
+            errorHandler(err, req, res)
+            return ;
+        }
+        res.json(users);
+    } catch (error) {
+        // Обработка ошибки
+        const err = new AppError('Internal Server Error', 500);
+        errorHandler(err, req, res)
+        // res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
 router.get('/:id', userService.getUserById);
+
+// router.get('/', (req, res) => {
+//     const id = req.params.id;
+//     // Теперь вы можете использовать переменную id для выполнения необходимых действий,
+//     // например, передать его в функцию userService.getUserByName()
+//     userService.getUserByName(id)
+//         .then(user => {
+//             // Обработка успешного выполнения запроса
+//             res.json(user);
+//         })
+//         .catch(error => {
+//             // Обработка ошибки
+//             res.status(500).json({ error: 'Internal Server Error' });
+//         });
+// });
 
 module.exports = router;
