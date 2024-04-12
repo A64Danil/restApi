6dc651c099508a5e0a73f8c263709296505fc11f
DanilZen <a64danil@mail.ru>
2024-04-12 11:31:47 +0200

    add getReturnedDataName to helpres

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,8 +6,18 @@ function lcFirst(string) {
     return string[0].toLowerCase() + string.slice(1);
 }
 
+function getReturnedDataName(funcName) {
+    const nameArr = Array.from(funcName);
+    const startIdx = nameArr.findIndex(c => c === c.toUpperCase())
+    let dataName = funcName.slice(startIdx);
+    const endIdx = Array.from(dataName).findIndex((c, i) => c === c.toUpperCase() && i !== 0)
+    if(endIdx !== -1) dataName = dataName.slice(0, endIdx);
+    return dataName;
+}
+
 
 module.exports = {
     ucFirst,
     lcFirst,
+    getReturnedDataName
 };
