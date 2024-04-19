37f7c748c4bf5fc092094d44ce897f3178b89cf0
DanilZen <a64danil@mail.ru>
2024-04-19 10:55:47 +0200

    delete TODO and productController

--- server/routes/apis/v1.js
+++ server/routes/apis/v1.js
@@ -3,16 +3,12 @@
 // const registerController = require('../../controllers/apis/register');
 // const loginController = require('../../controllers/apis/login');
 // const dashboardController = require('../../controllers/apis/dashboard');
-const productController = require('../../controllers/productController');
 const userController = require('../../controllers/userController');
 const express = require('express');
 
 let router = express.Router();
 
-// TODO: конкретно это место это жесть оно не так долджно работать
-router.use('/test', productController.getProducts)
 
-// TODO: подключить позднее
 // router.use('/register', registerController);
 router.use('/users', userController);
 // router.use('/login', loginController);
